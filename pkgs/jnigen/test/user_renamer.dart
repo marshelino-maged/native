eec4f6ef9ce9fec59ddbfd6000237e64b2b30bb7
Marshelino Maged <marshelinomaged@gmail.com>
2025-01-27 20:38:36 +0200

    edit test

--- pkgs/jnigen/test/user_renamer.dart
+++ pkgs/jnigen/test/user_renamer.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:jnigen/jnigen.dart';
+import 'package:jnigen/jnigen.dart' as jnigen;
 import 'package:jnigen/src/bindings/linker.dart';
 import 'package:jnigen/src/bindings/renamer.dart';
 import 'package:jnigen/src/elements/elements.dart' as ast;
@@ -11,6 +11,10 @@ import 'package:test/test.dart';
 
 extension on Iterable<ast.Method> {
   List<String> get finalNames => map((m) => m.finalName).toList();
+}
+
+extension on Iterable<ast.Param> {
+  List<String> get finalNames => map((p) => p.finalName).toList();
 }
 
 extension on Iterable<ast.Field> {
@@ -28,42 +32,43 @@ class UserRenamer extends Visitor {
 
   @override
   void visitMethod(Method method) {
-    if (method.name == 'Foo') {
-      method.newName = 'Bar';
+    if (method.name.contains('Foo')) {
+      method.newName = method.name.replaceAll('Foo', 'Bar');
     }
   }
 
   @override
   void visitField(Field field) {
-    if (field.name == 'Foo') {
-      field.newName = 'Bar';
+    if (field.name.contains('Foo')) {
+      field.newName = field.name.replaceAll('Foo', 'Bar');
     }
   }
 
   @override
   void visitParam(Param parameter) {
-    if (parameter.name == 'Foo') {
-      parameter.newName = 'Bar';
+    if (parameter.name.contains('Foo')) {
+      parameter.newName = parameter.name.replaceAll('Foo', 'Bar');
     }
   }
 }
 
 Future<void> rename(ast.Classes classes) async {
-  final config = Config(
-    outputConfig: OutputConfig(
-      dartConfig: DartCodeOutputConfig(
-        path: Uri.file('test.dart'),
-        structure: OutputStructure.singleFile,
+  final config = jnigen.Config(
+      outputConfig: jnigen.OutputConfig(
+        dartConfig: jnigen.DartCodeOutputConfig(
+          path: Uri.file('test.dart'),
+          structure: jnigen.OutputStructure.singleFile,
+        ),
       ),
-    ),
-    classes: [],
-  );
+      classes: []
+      );
   await classes.accept(Linker(config));
   classes.accept(Renamer(config));
 }
 
 void main() {
-  test('Exclude something using the user excluder, Simple AST', () async {
+  test('rename classes, fields, methods and params using user visitors',
+      () async {
     final classes = ast.Classes({
       'Foo': ast.ClassDecl(
         binaryName: 'Foo',
@@ -83,31 +88,33 @@ void main() {
         ],
       ),
       'y.Foo': ast.ClassDecl(
-          binaryName: 'y.Foo',
-          declKind: ast.DeclKind.classKind,
-          superclass: ast.TypeUsage.object,
-          methods: [
-            ast.Method(
-              name: 'Foo', 
-              returnType: ast.TypeUsage.object,
-              params: [
-                ast.Param(name: 'Foo', type: ast.TypeUsage.object),
-                ast.Param(name: 'Foo1', type: ast.TypeUsage.object),
-              ] 
-            ),
-          ],
-          ),
+        binaryName: 'y.Foo',
+        declKind: ast.DeclKind.classKind,
+        superclass: ast.TypeUsage.object,
+        methods: [
+          ast.Method(name: 'Foo', returnType: ast.TypeUsage.object, params: [
+            ast.Param(name: 'Foo', type: ast.TypeUsage.object),
+            ast.Param(name: 'Foo1', type: ast.TypeUsage.object),
+          ]),
+        ],
+      ),
     });
 
     final simpleClasses = Classes(classes);
     simpleClasses.accept(UserRenamer());
+
+    await rename(classes);
 
     expect(classes.decls['y.Foo']?.finalName, 'y.Bar');
     expect(classes.decls['Foo']?.finalName, 'Bar');
 
     expect(classes.decls['Foo']?.fields.finalNames,
         ['Bar', r'Bar$1', 'Bar1', r'Bar1$1']);
+
     expect(classes.decls['Foo']?.methods.finalNames,
         [r'Bar$2', r'Bar$3', r'Bar1$2', r'Bar1$3']);
+
+    expect(classes.decls['y.Foo']?.methods.first.params.finalNames,
+        ['Bar', 'Bar1']);
   });
 }
